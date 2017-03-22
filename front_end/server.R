15bb1eb5a187539d4d4542f1d0153151da05030d
RossKen <rosskennedy1994@gmail.com>
2017-03-22 09:18:57 +0000

    Update server.R

--- front_end/server.R
+++ front_end/server.R
@@ -24,8 +24,8 @@ function(input, output) {
                         d$Date <= input$a_date_range[2] ))
   }
 
-  output$dt <- renderDataTable({
-    
+  output$x1 <- renderDataTable({
+#### Progress Bar goes here    
     datatable(data = df()[,c('Date', 'Answer_Date', 'Cluster','Sim_Score')], #[c("Date", "Answer_Date","Cluster","Similarity Score")],
               colnames = c("Document #", "Question Date","Answer Date", "Cluster","Similarity Score"),
               class = 'display',
@@ -38,8 +38,42 @@ function(input, output) {
                              paging = FALSE)
     )
   })
-  output$plot <- renderPlot({
+  
+  
+  
+  output$x2 <- renderPlot({
+    #s = input$x1_rows_selected
+    #ar(mar=c(4,4,1,.1))
     plot(df()$Date,df()$Sim_Score,
          xlab = 'Question Date', ylab = 'Similarity Score')
+    #if (length(s)) points(Plot[s, , drop = FALSE], pch = 19, cex = 10)
       })
+  
+  dfClus = function(){
+    dplyr::filter(d, (d$Cluster == input$x3))
+  }
+  
+  output$x3 <- renderDataTable({
+    datatable(data = dfClus()[,c('Question_ID','Question_Text')],
+              options = list(scroller = TRUE,
+                             searching = FALSE,
+                             paging = FALSE))
+  }) 
+   output$x4 <- renderPlot({
+     p = ggplot(data=dfClus, aes(x=dfClus()$Date, y=dfClus()$Date))+geom_bar(stat = 'identity')
+     p
+   })
+   output$x5 <- renderDataTable({
+     datatable(data=d,
+               filter = 'top',
+               options = list(scroller = TRUE,
+                              paging = FALSE))
+   })
+   
+  output$x8 <- renderPlot({
+    p = ggplot(data = d, aes(x = d$date, y = count(d$date == x))+geom_line())
+    p
+  })
+   
+   
 }
