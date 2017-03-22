9f615eeb42b3fef3eba62f0a93fdf16f272d32c0
RossKen <rosskennedy1994@gmail.com>
2017-03-22 09:19:42 +0000

    Update ui.R

--- front_end/ui.R
+++ front_end/ui.R
@@ -4,13 +4,15 @@ source(file = "global.R")
 navbarPage("PQ Text Analysis",
   tabPanel("Similarity",
     fluidRow(
-      column(4,
+      column(6,
         textInput(inputId = "question", 
-                  label = "Search Text",                    
+                  label = "Search Text",
+                  width = '100%',
                   value = " To ask Her Majesty's Government whether any complaint has been made to the Judicial Conduct Investigations Office about the conduct of Mrs Justice Hogg in the case of Ellie Butler."
-                  )
+                  ),
+        actionButton("goButton", "Search")
             ),
-                     
+                   
       column(3,
         sliderInput(inputId = "q_date_range", 
                     label = "Question Date Range", 
@@ -21,7 +23,6 @@ navbarPage("PQ Text Analysis",
                     )
             ),
                      
-      column(1),
                      
       column(3,
             sliderInput(inputId = "a_date_range",   
@@ -35,11 +36,43 @@ navbarPage("PQ Text Analysis",
       ),
     fluidRow(
       column(6,
-             DT::dataTableOutput('dt')
+             dataTableOutput('x1')
              ),
-      column(6,plotOutput("plot")
+      column(6,plotOutput("x2", height = 500)
             )
     )),
-  tabPanel("Component 2"),
-  tabPanel("Component 3")
+  tabPanel("Cluster",
+           fluidRow(
+             column(12,
+             selectInput("x3", "Choose Cluster:",
+                         choices = unique(d$Cluster))
+             )
+           ),
+           fluidRow(
+             column(12,
+             plotOutput('x4')
+             )
+           ),
+           fluidRow(
+             column(12,
+             dataTableOutput("x3")
+             )
+           )
+           ),
+  
+  tabPanel("Q&A Analysis",
+           sidebarPanel(
+             selectInput(inputId ="x6",
+                         "Choose a Constituency:",
+                         choices = unique(d$MP_Constituency))#,
+             #selectInput("x7", "Choose an MP:",
+             #            choices = unique(MPChoice()))
+           ),
+           mainPanel(
+             plotOutput("x8")
+           )
+  ),
+  tabPanel("Data",
+           dataTableOutput('x5')
+           )
 )
