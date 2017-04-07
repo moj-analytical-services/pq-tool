7a25ed80d593f6321f4daef98d880454c4b7a848
Andrew Poulton <andrew.poulton@justice.gsi.gov.uk>
2017-04-07 11:16:20 +0000

    Barely-legible function to find nearest matches quickly, and data that helps it

--- PQskmeans.R
+++ PQskmeans.R
@@ -44,9 +44,16 @@ bSKmeans_start<-skmeans(t(search.space), k=1000, method = 'pclust', control = li
 proc.time()-ptm
 
 bigSKmeans2<-bSKmeans_start
+sparseSKmeans<-bigSKmeans2$prototypes
+sparseSKmeans[which(abs(sparseSKmeans)<0.01)]<-0
+
+sparseSKmeans<-skmeans(t(search.space), k=1000, method = 'pclust', control = list(maxiter =1, nruns = 1, start = sparseSKmeans, verbose = TRUE))
+
+shinyKlust<-MoJallPQsforTableau$Cluster
+shinySKmeans<-skmeans(t(lsaOut), k=1000, method = 'pclust', control = list(maxiter =1, maxchains = 1, start = shinyKlust, verbose = TRUE))
 
 
-v_names<-sappy(tdm_i, function(c) tdmDimNames[[1]][c])
+v_names<-sapply(tdm$i, function(c) tdmDimNames[[1]][c])
 space_names<-sapply(search.space$i, function(c) tdmDimNames[[1]][c])
 
 clust<-function(n){
@@ -54,11 +61,12 @@ clust<-function(n){
   return(t)
 }
 
-prot<- function(n,tol){
+prot<- function(n,tol, skProt){
   if(is.string(n)){
-    t<-bigSKmeans2$prototypes[which(bigSKmeans2$prototypes[,n]>tol),n]
+      print(n)
+    t<-skProt[which(skProt[,n]>tol),n]
   }else{
-    t<-bigSKmeans2$prototypes[n,which(bigSKmeans2$prototypes[n,]>tol)]
+    t<-skProt[n,which(skProt[n,]>tol)]
   }
   return(sort(t, decreasing = TRUE))
 }
@@ -69,19 +77,77 @@ doc<- function(n){
   t<-tdm$v[tdm$j==n]
   return(t)
 }
-
-test_query<-function(query, tol=1e-02){
-  query<-query%>%tolower()%>%removeWords(stopwords_en)%>%
+stopwordList <- c(
+    stopwords(),'a','b','c','d','i','ii','iii','iv',
+    'secretary','state','ministry','majesty',
+    'government','many','ask','whether',
+    'assessment','further','pursuant','justice',
+    'minister','steps','department','question'
+)
+test_query<-function(query, tol=1e-02, skProt = bigSKmeans2$prototypes){
+  query<-query%>%tolower()%>%removeWords(stopwordList)%>%
           strsplit(" ")%>%
           sapply(stemDocument)%>%
           (function(vec){
             return(vec[sapply(vec, function(x) x %in% v_names)])
           })
-  return(lapply(query, function(x) prot(x,tol)))
+  return(lapply(query, function(x){
+                                    k<-prot(x,tol, skProt)
+                                    return(k[which(k>0.1)])
+                                    }
+                                    ))
 }
 
+
+#Search space for query vector
+werdz<-search.space$dimnames[[1]]
+
+#Function to vectorize query
+queryVec<-function(query){
+    query<-query%>%tolower()%>%removePunctuation%>%
+        removeWords(stopwordList)%>%
+        strsplit(" ")%>%
+        sapply(stemDocument)%>%
+        (function(vec){
+            return(vec[sapply(vec, function(x) x %in% werdz)])
+        })
+    
+    return(which(werdz %in% query))
+}
+
+#Function to return top 30 matches - needs search.space.rda to run
+returnNearestMatches<-function(que, space = tdm){
+foundWords<-which(space$i %in% queryVec(que))
+#eyes<-search.space$i[foundWords]
+jays<-space$j[foundWords]
+vees <-space$v[foundWords]
+JayVees <- data.table(jays = jays, vees = vees)
+outGroup <- JayVees[, .("Vee_sum" = sum(vees)), by = jays ][order(-Vee_sum)]
+outGroup[1:30]
+}
 
 
 qtext<-'Example query with words in it.'
 qtext.stems <- tm_map(Corpus(VectorSource(qtext)),stemDocument)
 stemqText<-data.frame(text=unlist(sapply(qtext.stems, '[', 'content')), stringsAsFactors=F)
+
+
+sum(sapply(cands, 
+       function(d) {
+           sum(sapply(names(d), 
+                      function(x){
+                          length(clust(as.integer(x)))
+               
+           })
+    
+)})
+)
+names(search.space$v)<-sapply(search.space$i, function(x) search.space$dimnames[[1]][x])
+stmScProd<-function(x,y, mat = search.space){
+    
+}
+
+
+#### Karik being awesome
+library(data.table)
+
