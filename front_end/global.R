f57f97af5bd5538cdbea49de690b98f95a61e6b2
RossKen <rosskennedy1994@gmail.com>
2017-04-18 14:27:37 +0100

    Update global.R

--- front_end/global.R
+++ front_end/global.R
@@ -14,17 +14,17 @@ library(slam)
 # Define R_date date type - to read in Long Date format in csv
 setAs("character", "R_date", function(from) as.Date(from, "%d %B %Y"))
 setClass('R_date')
-myColClasses = c("Date" = "R_date",
+myColClasses <- c("Date" = "R_date",
                  "Answer_Date" = "R_date")
 
-rawData = read.csv('../Data/MoJallPQsforTableau.csv',colClasses = myColClasses)
-data = data.frame(rawData)
+rawData <- read.csv('../Data/MoJallPQsforTableau.csv',colClasses = myColClasses)
+data <- data.frame(rawData)
 data[is.na(data$MP_Constituency)] = "None"
-data["Question_MP"] = lapply(data["Question_MP"], function(x) { 
+data["Question_MP"] <- lapply(data["Question_MP"], function(x) { 
   gsub("Mr |Mrs |Ms ", "", x)
   })
 
-cluster_data = read.csv("../Data/topDozen.csv")
+cluster_data <- read.csv("../Data/topDozen.csv")
 
 #This loads stuff created by the DataCreator.R script
 
@@ -32,6 +32,6 @@ load(file = "lsaOut.rda")
 load(file = "tdm.rda")
 load(file = "klusters.rda")
 
-col_names = c('Document #', 'Question ID', 'Question', 'Answer', 'Question MP', 'Answer MP', 'Q Date','A Date', 'Cluster','Similarity Score')
+col_names <- c('Document #', 'Question ID', 'Question', 'Answer', 'Question MP', 'Answer MP', 'Q Date','A Date', 'Cluster','Similarity Score')
 
-merged_clusters = ddply(d, .(Date, Answer_Date, Cluster), summarize, Question_Text = paste0(Question_Text, collapse = " "))
+merged_clusters <- ddply(d, .(Date, Answer_Date, Cluster), summarize, Question_Text = paste0(Question_Text, collapse = " "))
