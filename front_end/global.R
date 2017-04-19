9d4da9dc26cd7948ab75690dc85abe0351b3a86c
RossKen <rosskennedy1994@gmail.com>
2017-04-19 12:08:38 +0100

    Update global.R

--- front_end/global.R
+++ front_end/global.R
@@ -22,6 +22,14 @@ data["Question_MP"] <- lapply(data["Question_MP"], function(x) {
 
 cluster_data <- read.csv("../Data/topDozen.csv")
 
+stopwordList <- c(
+  stopwords(),'a','b','c','d','i','ii','iii','iv',
+  'secretary','state','ministry','majesty',
+  'government','many','ask','whether',
+  'assessment','further','pursuant','justice',
+  'minister','steps','department','question'
+  
+  
 col_names <- c('Document #', 'Question ID', 'Question', 'Answer', 'Question MP', 'Answer MP', 'Q Date','A Date', 'Cluster','Similarity Score')
 
 merged_clusters <- ddply(data, .(Date, Answer_Date, Cluster), summarize, Question_Text = paste0(Question_Text, collapse = " "))
