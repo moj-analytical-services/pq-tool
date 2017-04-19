11ac38f5900d7861c13662d7af71bd0638c41178
Edward Andress <eddie_andress@hotmail.com>
2017-04-19 09:55:19 +0100

    Test- no error on page load + basic search works

--- tests/testthat/test-basic.R
+++ tests/testthat/test-basic.R
@@ -4,16 +4,23 @@ library(shiny)
 library(RSelenium)
 library(testthat)
 
-remDr 		<- remoteDriver(port = 4444)
+remDr       <- remoteDriver(port = 4444)
 remDr$open(silent = TRUE)
-sysDetails 	<- remDr$getStatus()
-browser 	<- remDr$sessionInfo$browserName
-appURL 		<- "http://127.0.0.1:8888"
+sysDetails  <- remDr$getStatus()
+browser     <- remDr$sessionInfo$browserName
+appURL      <- "http://127.0.0.1:8888"
 
 test_that("can connect to app", {  
   remDr$navigate(appURL)
   appTitle <- remDr$getTitle()[[1]]
   expect_equal(appTitle, "PQ Text Analysis")  
+})
+
+test_that("there are no errors on the page", {
+  expect_that(
+    remDr$findElement("css selector", ".shiny-output-error"),
+    throws_error('NoSuchElement')
+  )
 })
 
 remDr$close()
