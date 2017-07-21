2c0a7b44eea07839c8b48fdfa31de5aebff77ff6
Edward Andress <eddie_andress@hotmail.com>
2017-07-21 14:27:11 +0100

    Improve apiClient and data access in application

--- tests/testthat/test-api-client.R
+++ tests/testthat/test-api-client.R
@@ -7,16 +7,10 @@ library(jsonlite)
 library(stringr)
 
 expected_endpoint   <- "http://lda.data.parliament.uk/answeredquestions"
-dummy_data_filepath <- file.path(SHINY_ROOT, 'tests/testthat/examples', 'dummy_API_response.csv')
-cols                <- colnames(read_csv(dummy_data_filepath))
 answering_body      <- "AnsweringBody.=Ministry+of+Justice"
 date_filter         <- "min-answer.dateOfAnswer=2017-03-23"
 
-dummy_api_response <- function() {
-  csv <- read.csv(dummy_data_filepath)
-  colnames(csv) <- cols
-  csv
-}
+dummy_pqs_api_response <- readRDS(file.path(SHINY_ROOT, 'tests/testthat/examples/api-responses', 'response-full.rds'))
 
 dummy_member_api_response <- function() {
   party           <- 'Party'
@@ -34,7 +28,7 @@ context("api functions; ARCHIVE PRESENT")
 test_that("last_answer_date() returns date of most recent answer", {
   with_mock(
     `file.exists`     = function(filepath) { TRUE                              },
-    `readr::read_csv` = function(filepath) { readRDS('./examples/archive.rds') },
+    `readr::read_csv` = function(filepath) { readRDS('./examples/data/archive.rds') },
     expect_equal(last_answer_date(), ymd("2017-05-02"))
   )
 })
@@ -51,7 +45,7 @@ test_that("number_to_fetch() calls the API with the correct params", {
 		`last_answer_date`   = function()         { '2017-03-23' },
 		`jsonlite::fromJSON` = function(actual_API_call) {
 			expect_equal(actual_API_call, expected_API_call)
-      readRDS('./examples/update.rds')
+      readRDS('./examples/api-responses/response-update.rds')
 		},
     number_to_fetch()
 	)
@@ -62,7 +56,7 @@ test_that("number_to_fetch() returns the number of questions to fetch", {
   with_mock(
     `file.exists`        = function(filepath) { TRUE                             },
     `last_answer_date`   = function()         { '2017-03-23'                     },
-    `jsonlite::fromJSON` = function(...)      { readRDS('./examples/update.rds') },
+    `jsonlite::fromJSON` = function(...)      { readRDS('./examples/api-responses/response-update.rds') },
     expect_equal(number_to_fetch(), 978)
   )
 })
@@ -72,7 +66,7 @@ test_that("fetch_questions() calls the API with the correct params", {
   page_param        <- "_page=0"
   download_size     <- "_pageSize=1000"
   expected_API_call <- str_interp(
-    "${expected_endpoint}.csv?${date_filter}&${answering_body}&${download_size}&${page_param}"
+    "${expected_endpoint}.json?${date_filter}&${answering_body}&${download_size}&_sort=date&${page_param}"
   )
 
   with_mock(
@@ -82,9 +76,9 @@ test_that("fetch_questions() calls the API with the correct params", {
     `number_to_fetch`  = function()                    { 1000         },
     `update_archive`   = function(questions)           { NULL         },
     `party`            = function(member)              { 'party'      },
-    `readr::read_csv`  = function(actual_API_call) {
+    `jsonlite::fromJSON` = function(actual_API_call) {
       expect_equal(actual_API_call, expected_API_call)
-      dummy_api_response()
+      dummy_pqs_api_response
     },
     fetch_questions()
   )
@@ -101,7 +95,7 @@ test_that("number_to_fetch() calls the API with the correct params", {
     `file.exists`        = function(filepath) { FALSE },
     `jsonlite::fromJSON` = function(actual_API_call) { 
       expect_equal(actual_API_call, expected_API_call)
-      readRDS('./examples/all.rds')
+      dummy_pqs_api_response
     },
     number_to_fetch()
   )
@@ -111,8 +105,8 @@ test_that("number_to_fetch() returns the number of questions to fetch", {
 	
   with_mock(
 		`file.exists`        = function(filepath) { FALSE                         },
-		`jsonlite::fromJSON` = function(...)      { readRDS('./examples/all.rds') },
-		expect_equal(number_to_fetch(), 130444)
+		`jsonlite::fromJSON` = function(...)      { readRDS('./examples/api-responses/response-full.rds') },
+		expect_equal(number_to_fetch(), 7352)
 	)
 })
 
@@ -121,7 +115,7 @@ test_that("fetch_questions() calls the API with the correct params", {
   page_param        <- "_page=0"
   download_size     <- "_pageSize=1000"
   expected_API_call <- str_interp(
-    "${expected_endpoint}.csv?${answering_body}&${download_size}&${page_param}"
+    "${expected_endpoint}.json?${answering_body}&${download_size}&_sort=date&${page_param}"
   )
 
   with_mock(
@@ -131,9 +125,9 @@ test_that("fetch_questions() calls the API with the correct params", {
     `write_csv`       = function(questions, filepath) { NULL    },
     `update_archive`  = function(questions)           { NULL    },
     `party`           = function(member)              { 'party' },
-    `readr::read_csv` = function(actual_API_call) {
+    `jsonlite::fromJSON` = function(actual_API_call) {
       expect_equal(actual_API_call, expected_API_call)
-      dummy_api_response()
+      dummy_pqs_api_response
     },
 
     fetch_questions()
@@ -146,8 +140,8 @@ test_that("fetch_questions creates an archive file if one does not already exist
     `file.exists`     = function(filepath) { FALSE   },
     `number_to_fetch` = function()         { 1000    },
     `party`           = function(memeber)  { 'party' },
-    `readr::read_csv` = function(actual_API_call) {
-      dummy_api_response()
+    `jsonlite::fromJSON` = function(actual_API_call) {
+      dummy_pqs_api_response
     },
     `write_csv`      = function(questions, filepath) { NULL },
     `update_archive` = function(new_questions)       { NULL }, 
@@ -170,14 +164,14 @@ test_that("fetch_questions() downloads new questions and calls the update_archiv
     `number_to_fetch`   = function()         { 3000    },
     `file.create`       = function(filepath) { NULL    },
     `party`             = function(member)   { 'party' },
-    `readr::read_csv` = function(actual_API_call) {
-      dummy_api_response()
+    `jsonlite::fromJSON` = function(actual_API_call) {
+      dummy_pqs_api_response
     },
 
     `update_archive` = function(questions) {
       expect_equal(
         nrow(questions),
-        3000
+        1000
       )
     },
 
@@ -202,7 +196,7 @@ test_that('party() cannot retrieve party for members of HoL', {
     party('Viscount Someone'),
     party('Baroness Someone'),
     party('Earl someone'),
-    party('Marquess')
+    party('Marquess someone')
   )
 
   expect_true( all(hol_parties %in% 'Not found') )
@@ -243,39 +237,31 @@ test_that('party() calls the API with the correct params', {
 
 context('api response parser')
 
-html_present <- function(list) {
-  bools_list <- grepl('<.{1,2}>', list)
-  T %in% bools_list
-}
-
-test_that("html tags are removed from answer text and colnames are correct", {
-  initial_response <- read_csv(dummy_data_filepath)
-  initial_answers  <- initial_response$'answer > answer text'
-  parsed_response  <- parse_response(initial_response)
+test_that("colnames are correct", {
+  initial_response <- dummy_pqs_api_response
+  parsed_response  <- parse_response(initial_response$result$items)
   parsed_answers   <- parsed_response$Answer_Text
   actual_cols      <- colnames(parsed_response)
   expected_cols    <- c(
-    'Question_ID',
-    'Question_Text',
-    'Answer_Text',
     'Question_MP',
+    'Question_Text',
+    'Question_ID',
     'MP_Constituency',
+    'Question_Date',
+    'Answer_Text',
     'Answer_MP',
-    'Date',
     'Answer_Date'
   )
 
   expect_equal(actual_cols, expected_cols)
-  expect_true(html_present(initial_answers))
-  expect_false(html_present(parsed_answers))
 })
 
 context('update_archive')
 
 test_that("The archive is updated without duplcation", {
 
-  archive       <- readRDS('./examples/archive.rds')[1:10,]
-  new_questions <- readRDS('./examples/archive.rds')[5:14,]
+  archive       <- readRDS('./examples/data/archive.rds')[1:10,]
+  new_questions <- readRDS('./examples/data/archive.rds')[5:14,]
 
   with_mock(
     `read_csv` = function(filepath) { archive },
