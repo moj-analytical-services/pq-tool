30f5225a42d52473b839ab19bbacf358d483bb86
RossKen <admin@RossK-Macbook.local>
2017-09-29 15:48:08 +0100

    Updates intro to include new dropdown

--- ui.R
+++ ui.R
@@ -35,10 +35,6 @@ navbarPage("MoJ Parliamentary Analysis Tool",
                                typing some keywords (e.g. Prison Officers) or a new PQ into the search box 
                                below. You will get a ranked list of the 100 most similar past questions, and 
                                a visualisation showing when they were asked.")
-<<<<<<< HEAD
-                      )),
-
-=======
                       ),
                       column(2,
                              offset = 2,
@@ -52,8 +48,6 @@ navbarPage("MoJ Parliamentary Analysis Tool",
                                        "auto")
                       )
                       ),
-                    
->>>>>>> Feeding multiple datasets into shiny app (#153)
                     fluidRow(
                       column(4,
                              introBox(
@@ -64,7 +58,7 @@ navbarPage("MoJ Parliamentary Analysis Tool",
                                value = "",
                                placeholder = "Enter search text here"
                              ),
-                             data.step = 1,
+                             data.step = 2,
                              data.intro = "Type some keywords (e.g. Prison Officers) or a new PQ into this box."
                              ),
                              bsTooltip("question",
@@ -73,16 +67,15 @@ navbarPage("MoJ Parliamentary Analysis Tool",
                                        options = list(container = "body")
                              )
                       ),
-<<<<<<< HEAD
-
-                      column(3,
-=======
-                      
                       column(2,
->>>>>>> Feeding multiple datasets into shiny app (#153)
+                             introBox(
                              selectizeInput(inputId = "answering_body_choice",
                                             label = "Choose Answering Body:",
                                             choices = answering_bodies_lookup$Name
+                             ),
+                             data.step = 1,
+                             data.intro = "Choose an answering body",
+                             data.position = "right"
                              ),
                              bsTooltip("answering_body_choice",
                                          "Choose an answering body. Note: this changes the data for all of the pages in the tool.",
@@ -103,7 +96,7 @@ navbarPage("MoJ Parliamentary Analysis Tool",
                                condition = searchTextEntered,
                                introBox(
                                  uiOutput("date_input_ui"),
-                               data.step = 2,
+                               data.step = 3,
                                data.position = "right",
                                data.intro = "Pick a range of dates you want to consider (leave this alone to search all the questions we have)"
                                ),
@@ -125,18 +118,18 @@ navbarPage("MoJ Parliamentary Analysis Tool",
                                introBox(
                                introBox(
                                dataTableOutput("similarity_table"),
-                               data.step = 3,
+                               data.step = 4,
                                data.position = "right",
                                data.intro = "This table shows the top 100 PQs that are most similar to your search terms. <br> <br>
                                Click on one of the rows to see the question and answer text."),
-                               data.step = 6,
+                               data.step = 7,
                                data.position = "right",
                                data.intro = "The question you selected on the graph has now been opened in the table."),
-                               data.step = 7,
+                               data.step = 8,
                                data.position = "right",
                                data.intro = "You can see all the questions asked by this MP/peer by clicking the 'See all questions asked by' button. 
                                <br> <br> To continue, try it!"),
-                               data.step = 12,
+                               data.step = 13,
                                data.position = "right",
                                data.intro = "All the questions in our database have been grouped into topics by an algorithm. These topics have been given a
                                number and three 'Topic Keywords' to give an idea of what the topic is about.<br> <br> Click the 'View topic' button to look
@@ -149,85 +142,15 @@ navbarPage("MoJ Parliamentary Analysis Tool",
                                introBox(
                                introBox(
                                plotlyOutput("similarity_plot", height = 500),
-                               data.step = 4,
+                               data.step = 5,
                                data.position = "left",
                                data.intro = "This graph plots the PQs from the table and when they were asked. <br> <br> Each 
                                point represents a PQ, with the height showing how similar the question is to your search terms (higher = more similar)"),
-                               data.step = 5,
+                               data.step = 6,
                                data.position = "left",
                                data.intro = "The grey line shows an average of parliamentary interest in the search terms. <br> <br>
                                The red point is highlighting the question you previously chose from the table. <br><br> Try 
                                clicking another point to highlight instead.")
-<<<<<<< HEAD
-                      )
-                      )
-                    )
-           ),
-
-           ########################### Topic Tab
-           tabPanel("Topic Analysis",
-                    fluidRow(
-                      column(3,
-                             selectizeInput(inputId = "topic_choice",
-                                            label = "Choose Topic Number:",
-                                            choices = unique(data$Topic)),
-                             bsTooltip(
-                               "topic_choice",
-                               "Enter a topic number from the previous page.
-              You can do this by selecting a number from
-              the dropdown or simply type it in.",
-                               "right",
-                               options = list(container = "body")
-                             )
-                      ),
-                      column(1,
-                             offset = 7,
-                             actionButton(
-                               "explanation_button",
-                               "What do these topics mean?",
-                               class="btn btn-primary"
-                             )
-                      )
-                    ),
-
-                    conditionalPanel(
-                      condition = topicChosen,
-                      fluidRow(
-                        column(4, 
-                               introBox(
-                               plotOutput("wordcloud",
-                                          width = "auto"),
-                               data.step = 13,
-                               data.position = "right",
-                               data.intro = "This wordcloud shows the words that are most important
-                               in this topic.<br><br> The bigger the word, the more important it is.")
-                               ),
-                        column(8, 
-                               introBox(
-                                 plotOutput("topic_plot"),
-                                 data.step = 14, 
-                                 data.position = "left",
-                                 data.intro = "This plot shows when questions in the topic were asked. <br> Each bar 
-                                 shows the number of questions asked in a particular fortnight - the higher the bar, 
-                                 the more questions from that topic.")
-                      )),
-                      fluidRow(
-                        introBox(
-                        introBox(
-                        dataTableOutput("topic_documents"),
-                        data.step = 15,
-                        data.position = "right",
-                        data.intro = "This table contains all of the information on the questions asked on this topic.<br><br>
-                        Click on a row to see the corresponding question and answer text."),
-                        data.step = 16,
-                        data.position = "top",
-                        data.intro = "That's it! You have made it to the end of the tutorial! <br><br> We hope this was useful. If you have any
-                        feedback on this tutorial, or the tool in general, please see the link at the bottom of the page.")
-                      )
-                    )
-           ),
-
-=======
                                )
                              )
                       )),
@@ -264,7 +187,7 @@ navbarPage("MoJ Parliamentary Analysis Tool",
                        column(4,
                               introBox(
                               plotOutput("wordcloud"),
-                              data.step = 13,
+                              data.step = 14,
                               data.position = "right",
                               data.intro = "This wordcloud shows the words that are most important
                               in this topic.<br><br> The bigger the word, the more important it is.")
@@ -272,7 +195,7 @@ navbarPage("MoJ Parliamentary Analysis Tool",
                        column(8,
                               introBox(
                                 plotOutput("topic_plot"),
-                                data.step = 14,
+                                data.step = 15,
                                 data.position = "left",
                                 data.intro = "This plot shows when questions in the topic were asked. <br> Each bar
                                 shows the number of questions asked in a particular fortnight - the higher the bar,
@@ -283,11 +206,11 @@ navbarPage("MoJ Parliamentary Analysis Tool",
                        introBox(
                        introBox(
                        dataTableOutput("topic_documents"),
-                       data.step = 15,
+                       data.step = 16,
                        data.position = "right",
                        data.intro = "This table contains all of the information on the questions asked on this topic.<br><br>
                        Click on a row to see the corresponding question and answer text."),
-                       data.step = 16,
+                       data.step = 17,
                        data.position = "top",
                        data.intro = "That's it! You have made it to the end of the tutorial! <br><br> We hope this was useful. If you have any
                        feedback on this tutorial, or the tool in general, please see the link at the bottom of the page.")
@@ -296,7 +219,6 @@ navbarPage("MoJ Parliamentary Analysis Tool",
            ),
 
 
->>>>>>> Feeding multiple datasets into shiny app (#153)
            ########################### Member tab
            tabPanel("Member Analysis",
                     fluidRow(
@@ -326,9 +248,8 @@ navbarPage("MoJ Parliamentary Analysis Tool",
                     fluidRow(
                       column(4,
                              introBox(
-                             plotOutput("member_wordcloud",
-                                        width = "auto"),
-                             data.step = 8,
+                             plotOutput("member_wordcloud"),
+                             data.step = 9,
                              data.position = "right",
                              data.intro = "This wordcloud shows the words that are most important in the questions
                              asked by this member.<br><br> The bigger the word, the more important it is.")
@@ -336,7 +257,7 @@ navbarPage("MoJ Parliamentary Analysis Tool",
                       column(8,
                              introBox(
                              plotOutput("member_plot"),
-                             data.step = 9,
+                             data.step = 10,
                              data.position = "left",
                              data.intro = "This plot shows when questions were asked by the selected member. <br><br>
                              The x axis shows the date when questions were asked and the y axis shows the count of questions asked on that date.")
@@ -347,11 +268,11 @@ navbarPage("MoJ Parliamentary Analysis Tool",
                        introBox(
                        introBox(
                         dataTableOutput("member_table"),
-                        data.step = 10,
+                        data.step = 11,
                         data.position = "top",
                         data.intro = "This table contains all of the information on the questions asked by this member.<br><br>
                         Click on a row to see the corresponding question and answer text."),
-                      data.step = 11,
+                      data.step = 12,
                       data.position = "top",
                       data.intro = "You can now navigate back to the first page by clicking on the 'Back to Search' button.")
            )
