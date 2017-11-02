01522d8c27470211cb805c4e96c731858088bd99
Sam Tazzyman <samtazzyman@Sams-MacBook-Pro.local>
2017-11-02 17:26:58 +0000

    Updated data on 2/11/17 Also added functionality to check whether remote archive is the same as local archive, to ensure that no questions have been missed Also rewrote README

--- tests/testthat/examples/data/generateTestData.R
+++ tests/testthat/examples/data/generateTestData.R
@@ -1,4 +1,5 @@
 library(tm)
+library(dplyr)
 
 #need to source DataCreator functions
 
@@ -10,3 +11,15 @@ cleancorpus <- cleanCorpus(corpus)
 
 saveRDS(corpus, file = "./examples/data/corpus.rda")
 saveRDS(cleancorpus, file = "./examples/data/cleaned_corpus.rda")
+
+
+origl <- read.csv("./examples/data/archived_pqs.csv", stringsAsFactors = F)
+
+both <- left_join(origl,
+                  origl,
+                  by = c("Question_ID",
+                         "Answer_Date"),
+                  suffix = c(".remote", ".local")) %>%
+  unique()
+
+write.csv(both, file = "./examples/data/TestQsData.csv")
